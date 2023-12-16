245ba4f16f7f9b4e18d78cbd702bdb858bb8e528
MarlissonMonte <92329431+MarlissonMonte@users.noreply.github.com>
2023-12-16 16:51:28 -0300

    script for type

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const element = document.querySelector("#text");
+const text = "Hello world, Sou Marlisson!"
+const interval = 180;
+
+function showText(element, text, interval){
+
+    const char = text.split("").reverse();
+
+    console.log(char);
+
+    const type = setInterval(() => {
+       if(!char.length){
+        return clearInterval(type);
+       } 
+
+       const next = char.pop();
+
+       element.innerHTML += next; 
+    }, interval);
+
+}
+
+showText(element, text, interval);
