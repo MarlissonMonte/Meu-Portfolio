4462ce295c2d7b923b766b56e122e43ede3f072a
MarlissonMonte <92329431+MarlissonMonte@users.noreply.github.com>
2024-06-17 23:58:51 -0300

    mudando o css

---
+++ js/dist/menu.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var hamburger = document.querySelector(".hamburger");
+var nav = document.querySelector(".nav");
+hamburger.addEventListener("click", function () {
+  return nav.classList.toggle("active");
+});
+//# sourceMappingURL=menu.dev.js.map
